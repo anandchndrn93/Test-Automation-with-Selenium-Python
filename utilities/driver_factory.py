641c036d953b2aca7e50cd167ee7d9681bd6c841
anandchndrn93 <nanduleo93@gmail.com>
2024-03-24 17:32:32 +0530

    Update driver

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -1,11 +1,13 @@
-import platform
+from sys import platform
 
 from selenium import webdriver
+from selenium.webdriver.chromium.service import ChromiumService
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.chrome.service import Service as ChromService
 from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.ie.service import Service as IEService
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.core.os_manager import ChromeType
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
@@ -18,7 +20,7 @@ class DriverFactory:
     def __init__(self):
         self.driver = None
 
-    def setup_driver(self,  headless,browser="chrome"):
+    def setup_driver(self, headless, browser="chrome"):
         if browser == "firefox":
             options = Options()
             options.headless = headless
@@ -30,8 +32,9 @@ class DriverFactory:
             if headless:
                 options.add_argument("--headless")
                 options.add_argument("window-size=1920,1080")
-            if platform.system() == "Linux":
-                self.driver = webdriver.Chrome(options=options, service=ChromService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+            if platform == "linux" or platform == "linux2":
+                self.driver = webdriver.Chrome(options=options, service=ChromiumService(
+                    ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
             else:
                 self.driver = webdriver.Chrome(options=options, service=ChromService(ChromeDriverManager().install()))
         elif browser == "edge":
